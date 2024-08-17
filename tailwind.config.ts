1d7a32cfa87fa3a52f9b81ce69b2270cf07572bc
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-17 10:10:37 -0500

    🆕 Variables de colores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,21 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      },
+      colors: {
+        "bg-100": "#08040a",
+        "principal": "#6c63ff",
+        "secondary-100": "#195fbf",
+        "secondary-200": "#2096f4",
+        "text-100": "#ffffff",
+        "text-200": "#dcdcdc",
+        "text-300": "#c3c3c3",
+        "bg-200": "#131213",
+        "bg-300": "#232323",
+        "bg-card": "#08040a",
+        "nav": "#08040a"
       }
+
     }
   },
   plugins: []
