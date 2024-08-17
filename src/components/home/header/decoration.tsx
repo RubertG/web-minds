91a37bb7b5496e970c7008dacd0aae1ff201ca87
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-17 14:45:52 -0500

    🛠️ Animacion de entrada de las decoraciones

--- src/components/home/header/decoration.tsx
+++ src/components/home/header/decoration.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export const Decoration = () => {
   return (
-    <div className="relative -z-10">
+    <div className="relative -z-10 fade-in-opacity">
       <Image
         className="absolute -translate-y-[50%] top-0 left-1/2 -translate-x-1/2 lg:w-[848px] select-none"
         src="/decoration.svg"
@@ -10,7 +10,7 @@ export const Decoration = () => {
         width={900}
         height={800} />
       <Image
-        className="absolute min-w-[1000px] md:w-[400px] 2xl:min-w-[1410px] left-1/2 -translate-x-1/2 -top-10 fade-in-opacity"
+        className="absolute min-w-[1000px] md:w-[400px] 2xl:min-w-[1410px] left-1/2 -translate-x-1/2 -top-10"
         src="/grid-principal.svg"
         alt="Logo de WebMinds"
         width={1410}
