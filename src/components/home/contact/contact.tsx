04fe1445523d9589c06826f41feefc18f9c4bc03
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-18 18:54:11 -0500

    🆕 Sobra decorativa

--- src/components/home/contact/contact.tsx
+++ src/components/home/contact/contact.tsx
@@ -11,21 +11,29 @@ export const Contact = ({
   return (
     <main
       id="contacto"
-      className={`min-h-screen relative py-16 lg:py-24 ${className}`}
+      className={`min-h-screen relative py-16 lg:py-24 flex flex-col justify-center ${className}`}
     >
       <Decoration />
       <h2
         className={`${bebasNeue.className} text-5xl text-text-100 font-bold text-center`}
       >¡Contáctanos!</h2>
-      <section className="md:grid md:grid-cols-2 md:items-center md:gap-4 mt-10">
+      <section className="grid md:grid-cols-2 md:items-center gap-7 md:gap-4 mt-10">
         <ContactForm />
-        <Image
-          className="justify-self-end"
-          src="/contacto.svg"
-          alt="Contacto"
-          width={500}
-          height={500}
-        />
+        <div className="relative justify-self-end">
+          <Image
+            className="absolute -translate-y-[50%] top-1/2 left-1/2 -translate-x-1/2 -z-20 opacity-70"
+            src="/decoration.svg"
+            alt="Decoración"
+            width={700}
+            height={700}
+          />
+          <Image
+            src="/contacto.svg"
+            alt="Contacto"
+            width={500}
+            height={500}
+          />
+        </div>
       </section>
     </main>
   )
